85edb5d49c3a28615c679ce6e05d914e974d6f00
Debayan Basu <debayanbasu11@gmail.com>
2020-10-23 19:00:15 +0530

    Committing existing blockrun codebase to Git

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public float restartDelay = 1f;
+    bool gameHasEnded = false;
+    public GameObject completeLevelUI;
+
+    public void EndGame(){
+        if(gameHasEnded == false){
+            gameHasEnded = true;
+            Debug.Log("Game Over!");
+            Invoke("Restart",restartDelay);
+        }
+    }
+
+    void Restart(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void CompleteLevel(){
+        Debug.Log("Level Won!");
+        completeLevelUI.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 3000f;
+    public float sideForce = 50f;
+
+    // Start is called before the first frame update
+    void Start(){
+        Debug.Log("Hello World!");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate(){
+
+        // Applying a force on Z axis so that the box continues to move forward
+        rb.AddForce(0,0,forwardForce*Time.deltaTime);
+
+        if(Input.GetKey("d")){
+            rb.AddForce(sideForce*Time.deltaTime,0,0, ForceMode.VelocityChange);
+        }
+
+         if(Input.GetKey("a")){
+            rb.AddForce(-sideForce*Time.deltaTime,0,0, ForceMode.VelocityChange);
+        }
+
+        if(rb.position.y < -1f){
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
